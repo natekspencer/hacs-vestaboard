1700dc2bdc3c8a78135ecbdcde9211f383bb63b6
lordratner <49489398+lordratner@users.noreply.github.com>
2025-10-01 11:20:37 -0500

    Update sensor.py
    Added new entity for alert expiration as diagnostic

--- custom_components/vestaboard/sensor.py
+++ custom_components/vestaboard/sensor.py
@@ -1,9 +1,14 @@
 """Vestaboard sensor entity."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from datetime import datetime
+
+import homeassistant.util.dt as dt_util
+
+from homeassistant.components.sensor import SensorDeviceClass, SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
@@ -12,13 +17,26 @@ from .entity import VestaboardEntity
 
 MESSAGE = SensorEntityDescription(key="message", name="Message")
 
+ALERT_EXPIRATION = SensorEntityDescription(
+    key="alert_expiration",
+    name="Alert Expiration",
+    device_class=SensorDeviceClass.TIMESTAMP,
+    entity_category=EntityCategory.DIAGNOSTIC,
+)
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up Vestaboard sensors using config entry."""
     coordinator: VestaboardCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([VestaboardSensorEntity(coordinator, entry, MESSAGE)])
+    async_add_entities(
+        [
+            VestaboardSensorEntity(coordinator, entry, MESSAGE),
+            VestaboardAlertExpirationSensorEntity(
+                coordinator, entry, ALERT_EXPIRATION
+            ),
+        ]
+    )
 
 
 class VestaboardSensorEntity(VestaboardEntity, SensorEntity):
@@ -28,3 +46,16 @@ class VestaboardSensorEntity(VestaboardEntity, SensorEntity):
     def native_value(self) -> str | None:
         """Return the value reported by the sensor."""
         return self.coordinator.message
+
+class VestaboardAlertExpirationSensorEntity(VestaboardEntity, SensorEntity):
+    """Vestaboard alert expiration sensor entity."""
+
+    @property
+    def native_value(self) -> datetime | None:
+        """Return the value reported by the sensor."""
+        if (
+            self.coordinator.alert_expiration is not None
+            and self.coordinator.alert_expiration > dt_util.now()
+        ):
+            return self.coordinator.alert_expiration
+        return None
